bba876e799a0324f16a1752aa69778131552c3a1
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-06-11 11:20:44 +0800

    Added a custom component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomBlockComponent } from './custom-block/custom-block.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomBlockComponent
   ],
   imports: [
     BrowserModule,
