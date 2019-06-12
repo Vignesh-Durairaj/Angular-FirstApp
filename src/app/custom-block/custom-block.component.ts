fca05e09caa97f59ee6eccd60035769cf8674106
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-06-12 10:17:34 +0800

    Added some event handling exercises

--- src/app/custom-block/custom-block.component.ts
+++ src/app/custom-block/custom-block.component.ts
@@ -3,10 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-custom-block',
   template: `
+    <div (click)=clear()>
     <h2>Hello. {{ blockTitle }}</h2>
     <ul>
-      <li *ngFor="let employee of employees">{{ employee.name }}<{{ employee.shortName }}> -- {{ employee.almaMater }}</li>
+      <li *ngFor="let emp of employees" (mouseover)=showEmployee(emp)>
+        {{ emp.name }}<{{ emp.shortName }}> -- {{ emp.almaMater }}
+      </li>
     </ul>
+    <span *ngIf=employeeDetails>{{ employeeDetails }}</span>
+    </div>
   `, 
   styleUrls: ['./custom-block.component.css']
 })
@@ -14,6 +19,7 @@ export class CustomBlockComponent implements OnInit {
 
   blockTitle: String;
   employees: object;
+  employeeDetails: String;
 
   constructor() {
     this.blockTitle = `This is a new-custom element created!`;
@@ -34,6 +40,13 @@ export class CustomBlockComponent implements OnInit {
     ];
    }
 
+   showEmployee(employee) {
+    this.employeeDetails = `Employee : ${employee.name}, who is also known to be ${employee.shortName} has been graduated from "${employee.almaMater}"`;
+   }
+
+   clear() {
+     this.employeeDetails = '';
+   }
   ngOnInit() {
   }
 
